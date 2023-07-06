340fa6ba25584af03c56cc7fa1987c02fcfb77f4
JoseManuelBarba <josemanuelbarbasanchez@gmail.com>
2023-07-06 09:57:55 +0200

    init project

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  final int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'API Conexion',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
+    );
+  }
+}
